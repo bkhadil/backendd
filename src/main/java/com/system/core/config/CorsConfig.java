fde5280b5a16a744e7950f2ca7c1b8228ae82c63
bkkhadil <boukadidahadil69@gmail.com>
2025-05-18 22:37:24 -1100

    Add cu

--- src/main/java/com/system/core/config/CorsConfig.java
+++ src/main/java/com/system/core/config/CorsConfig.java
@@ -21,7 +21,9 @@ public class CorsConfig {
         "http://localhost*",
         "http://frontend*",
         "http://angularpart:*",
-        "null"
+        "null",
+        "https://frontend-production-3f78.up.railway.app",
+        "frontend-production-3f78.up.railway.app"
       ));
 
         config.addAllowedMethod("*"); // Autoriser toutes les méthodes
